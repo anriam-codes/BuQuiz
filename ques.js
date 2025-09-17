f17310d2c7ea4cee569a97c9e92eb13915c0502c
Krithik Sriharsha <Krithik>
2025-09-17 08:51:35 +0530

    Initial commit - quiz app

---
+++ ques.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+      numb: 1,
+      question: "What is the capital of Australia?",
+      answer: "C",
+      options: [
+        "A. Sydney",
+        "B. Melbourne",
+        "C. Canberra",
+        "D. Brisbane"
+      ]
+    },
+    {
+      numb: 2,
+      question: "Which planet is known as the Red Planet?",
+      answer: "A",
+      options: [
+        "A. Mars",
+        "B. Venus",
+        "C. Jupiter",
+        "D. Saturn"
+      ]
+    },
+    {
+      numb: 3,
+      question: "Who painted the Mona Lisa?",
+      answer: "A",
+      options: [
+        "A. Leonardo da Vinci",
+        "B. Michelangelo",
+        "C. Raphael",
+        "D. Pablo Picasso"
+      ]
+    },
+    {
+      numb: 4,
+      question: "What is the largest country in the world by land area?",
+      answer: "A",
+      options: [
+        "A. Russia",
+        "B. China",
+        "C. Canada",
+        "D. United States"
+      ]
+    },
+    {
+      numb: 5,
+      question: "Which element is the most abundant in the Earth's atmosphere?",
+      answer: "B",
+      options: [
+        "A. Oxygen",
+        "B. Nitrogen",
+        "C. Carbon Dioxide",
+        "D. Argon"
+      ]
+    }
+  ];
